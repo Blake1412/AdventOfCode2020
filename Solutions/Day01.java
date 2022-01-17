5f94e59eafb4194287004b387e76621adf8d0d45
Blake1412 <blakeryan30@gmail.com>
2022-01-17 01:59:29 +0000

    Inital commit + Day01 + Day02

--- Solutions/Day01.java
+++ Solutions/Day01.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Day01 {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day01 {
+    ArrayList<Integer> data = new ArrayList<>();
+
+    public Day01() throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("data.txt"));
+        while (scan.hasNext()) {
+            data.add(scan.nextInt());
+        }
+        scan.close();
+    }
+
+    public int part1() {
+        int answer = 0;
+        for (int i = 0; i < data.size(); i++) {
+            for (int j = i + 1; j < data.size(); j++) {
+                if (data.get(i) + data.get(j) == 2020) {
+                    answer = data.get(i) * data.get(j);
+                }
+            }
+        }
+        return answer;
+    }
+
+    public int part2() {
+        int answer = 0;
+        for (int i = 0; i < data.size(); i++) {
+            for (int j = i + 1; j < data.size(); j++) {
+                for (int k = j + 1; k < data.size(); k++) {
+                    if (data.get(i) + data.get(j) + data.get(k) == 2020) {
+                        answer = data.get(i) * data.get(j) * data.get(k);
+                    }
+                }
+            }
+        }
+        return answer;
+    }
 }
--- Solutions/Day02.java
+++ Solutions/Day02.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class Day02 {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day02 {
+    ArrayList<String> frequencies = new ArrayList();
+    ArrayList<Character> characters = new ArrayList();
+    ArrayList<String> passwords = new ArrayList();
+
+
+    public Day02() throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("data.txt"));
+        while (scan.hasNextLine()) {
+            String st = scan.nextLine();
+            frequencies.add(st.split(": ")[0].split(" ")[0]);
+            characters.add(st.split(": ")[0].split(" ")[1].charAt(0));
+            passwords.add(st.split(": ")[1]);
+        }
+        scan.close();
+    }
+
+    public int part1() {
+        int answer = 0;
+
+        for (int i = 0; i < passwords.size(); i++) {
+            int count = 0;
+            for (int j = 0; j < passwords.get(i).length(); j++) {
+                if (passwords.get(i).charAt(j) == characters.get(i)) {
+                    count++;
+                }
+            }
+            if (count >= Integer.valueOf(frequencies.get(i).split("-")[0]) && count <= Integer.valueOf(frequencies.get(i).split("-")[1])) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public int part2() {
+        int answer = 0;
+
+        for (int i = 0; i < passwords.size(); i++) {
+            if (passwords.get(i).charAt(Integer.valueOf(frequencies.get(i).split("-")[0]) - 1) == characters.get(i) ^ passwords.get(i).charAt(Integer.valueOf(frequencies.get(i).split("-")[1]) - 1) == characters.get(i)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
 }
