e15f416d4516fda071656ff540357ca84fdd2b1f
Blake1412 <blakeryan30@gmail.com>
2022-01-20 03:05:32 +0000

    Day 13

--- Solutions/Day13.java
+++ Solutions/Day13.java
@@ -3,18 +3,61 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Day13 {
-    int arrivalTime = 1003240;
-    List<Integer> busIntervals;
+    final int arrivalTime = 1003240;
+    List<String> buses;
+    List<Integer> busIDs = new ArrayList<>(), departurePositions = new ArrayList<>(), busIntervals;
 
     Day13() throws FileNotFoundException {
         Scanner scan = new Scanner(new File("data.txt"));
-        busIntervals = new ArrayList<>(Arrays.stream(scan.nextLine().split(",")).filter(x -> x.matches("\\d+")).mapToInt(Integer::parseInt).boxed().toList());
+        buses = new ArrayList<>(Arrays.asList(scan.nextLine().split(",")));
 
+        int count = 0;
+        for (String bus : buses) {
+            if (!bus.equals("x")) {
+                busIDs.add(Integer.parseInt(bus));
+                departurePositions.add(count);
+            }
+            count++;
+        }
+
+        busIntervals = new ArrayList<>(buses.stream()
+                .filter(x -> x.matches("\\d+"))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .toList());
     }
 
     public int part1() {
-        int busArrivalTime = busIntervals.stream().map(i -> (arrivalTime / i + 1) * i).reduce(Integer::min).orElse(0);
-        int busID = busIntervals.stream().filter(x -> busArrivalTime % x == 0).findFirst().orElse(0);
+        int busArrivalTime = busIntervals.stream()
+                .map(i -> (arrivalTime / i + 1) * i)
+                .reduce(Integer::min)
+                .orElse(0);
+        int busID = busIntervals.stream()
+                .filter(x -> busArrivalTime % x == 0)
+                .findFirst()
+                .orElse(0);
+
         return busID * (busArrivalTime - arrivalTime);
+    }
+
+    private boolean nextSyncedBus(long startPos, int syncedBuses) {
+        if (startPos == 0) {
+            return false;
+        }
+        return ((startPos + departurePositions.get(syncedBuses)) % busIDs.get(syncedBuses) == 0);
+    }
+
+    public Long part2() {
+        long timestamp = busIDs.get(0), timestampInterval = busIDs.get(0);
+        int syncedBuses = 1;
+
+        while (syncedBuses < busIDs.size()) {
+            while (!nextSyncedBus(timestamp, syncedBuses)) {
+                timestamp += timestampInterval;
+            }
+            timestampInterval = timestampInterval * busIDs.get(syncedBuses);
+            syncedBuses++;
+        }
+        return timestamp;
     }
 }
